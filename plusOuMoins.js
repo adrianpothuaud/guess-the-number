5eee99cdc69991ba30ce15f84348b48fe64b588d
adrianpothuaud <adrianpothuaud@protonmail.com>
2020-02-26 01:05:04 +0100

    First commit

--- plusOuMoins.js
+++ plusOuMoins.js
@@ -1,59 +1,105 @@
+// ./plusOuMoins.js
+
+/*
+
+    Définition des fonctions pour le jeu
+
+*/
+
+/**
+ * Mise à jour temps restant
+ */
 function setTimeLeftOnPage() {
 
+    // On récupère l'élément dans lequel on affiche le temps restant
+    // On insère le temps restant
     $('#timeLeftValue').html(timeLeft);
 }
 
+/**
+ * Evaluation d'un essai
+ */
 function evaluateResponse() {
 
     console.log("👀 Évaluation de la réponse 👀");
 
+    // On récupère la valeur saisie
     const response = $('#guessInput').val();
-
     console.log("👉 Réponse : " + response);
 
+    // Conditions sur la réponse
     if(response < mystery) {
+        // Si nombre saisie trop petit ...
         itsMore();
     } else if (response > mystery) {
+        // Si nombre saisie trop grand ...
         itsLess();
     } else {
+        // C'est gagné !
         youWin();
     }
 }
 
+/**
+ * Réglages des affichages lorsque le nombre saisie est trop petit
+ */
 function itsMore() {
 
     console.log("👍 Give me more 👍")
 
+    // On cache les indices précédents
     $('#less').hide();
+    // On affiche l'indice adéquat
     $('#more').show();
 }
 
+/**
+ * Réglages des affichages lorsque le nombre saisie est trop grand
+ */
 function itsLess() {
 
     console.log("👎 Not so much 👎")
 
+    // On cache les indices précédents
     $('#more').hide();
+    // On affiche l'indice adéquat
     $('#less').show();
 }
 
+/**
+ * Réglages d'affichages et fin du jeu quand la partie est gagnée
+ */
 function youWin() {
-
-    clearInterval(timeLeftInterval);
 
     console.log("👏 We have a WINNER !!! 👏")
 
-    $('#timeLeftValue').css('color', 'grey');
+    // On supprime l'interval de temps
+    clearInterval(timeLeftInterval);
+
+    // On désactive le champs et le bouton
     $('#guessInput').prop('disabled', true);
     $('#submitBtn').prop('disabled', true);
+
+    // On enlève les indices et autres textes qui ne sont plus utiles
     $('#nok').hide();
     $('#info').hide();
-    $('#guessInputLabel').hide();
-    $('#ok').show();
     $('#more').hide();
     $('#less').hide();
+    $('#guessInputLabel').hide();
+
+    // On met le temps restant en gris
+    $('#timeLeftValue').css('color', 'grey');
+
+    // On affiche le message gagnant
+    $('#ok').show();
+    
+    // On affiche le nombre mystère
     $('#resText').show();
 }
 
+/**
+ * Réglages d'affichages et fin du jeu quand la partie est perdue
+ */
 function gameOver() {
 
     clearInterval(timeLeftInterval);
