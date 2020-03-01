93d5a9f23032fc5cce5a594f020820fdb8a74bff
adrianpothuaud <adrianpothuaud@protonmail.com>
2020-03-01 01:21:49 +0100

    Finished test suites

--- cypress/integration/guess-the-number/v1.visual.spec.js
+++ cypress/integration/guess-the-number/v1.visual.spec.js
@@ -1,36 +1,36 @@
 /// <reference types="cypress" />
 
-context('Jeu du Nombre Mystère - v1', () => {
+context('[Ft02] Jeu du Nombre Mystère - VISUEL - v1', () => {
 
     beforeEach(() => {
 
         cy.visit('http://localhost:8080/')
     });
 
-    it('Le jeu par défaut a le même look', () => {
+    it('[Sp14] Le jeu par défaut a le même look', () => {
 
         cy.compareSnapshot('default', 0.0);
     });
 
-    it('Le jeu gagné a le même look', () => {
+    it('[Sp15] Le jeu gagné a le même look', () => {
 
         cy.youWin();
         cy.compareSnapshot('gagné', 0.0);
     });
 
-    it('Le jeu perdu a le même look', () => {
+    it('[Sp16] Le jeu perdu a le même look', () => {
 
         cy.gameOver();
         cy.compareSnapshot('perdu', 0.0);
     });
 
-    it('Le jeu après nombre saisie trop petit a le même look', () => {
+    it('[Sp17] Le jeu après nombre saisie trop petit a le même look', () => {
 
         cy.tooLow();
         cy.compareSnapshot('trop-petit', 0.0);
     });
 
-    it('Le jeu après nombre saisie trop grand a le même look', () => {
+    it('[Sp18] Le jeu après nombre saisie trop grand a le même look', () => {
 
         cy.tooHigh();
         cy.compareSnapshot('trop-grand', 0.0);
